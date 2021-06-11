6d96da9f243854606b4bccdc8c27c8ee31f79cf9
maaz ismail <MY_NAME@example.com>
2021-06-11 23:33:08 +0500

    final commit by 2018296

---
+++ app/src/main/java/com/mcproject/chatapp/UserClient.java
@@ -0,0 +1,19 @@
+package com.mcproject.chatapp;
+
+import android.app.Application;
+
+import com.mcproject.chatapp.models.User;
+
+public class UserClient extends Application {
+
+    private User user = null;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+}
